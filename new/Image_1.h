77d61c1c425124eedc423d9fa827ba8a2585a513
Nikhil Kaniyeri <69250757+kaniyeri@users.noreply.github.com>
2022-03-09 22:53:12 +0530

    final

--- new/Image_1.h
+++ new/Image_1.h
@@ -1,5 +1,5 @@
 #include "cortos.h"
 
-#define LENGTH 1280
-#define BREADTH 1024
-short arr_out_img[BREADTH + 2][LENGTH + 2];
+#define LENGTH 4
+#define BREADTH 4 
+short arr_out_img[BREADTH + 2][LENGTH + 2] = [[20,20,20,0],[20,20,20,0],[20,20,20,0],[0,0,0,0]];
